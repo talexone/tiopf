73e3c742040e6d62f0c31681e8a800883c98e8f0
Alexandre Tihonovs <talexone@gmail.com>
2022-04-01 09:08:04 +0200

    OID as object

--- Core/tiObject.pas
+++ Core/tiObject.pas
@@ -418,7 +418,7 @@ type
    {$IFDEF OID_AS_INT64}
       property    OID        : TtiOID                   read GetOID write SetOID;
    {$ELSE}
-      property    OID        : TtiOID                   read GetOID write SetOID;      ;
+      property    OID        : TtiOID                   read GetOID;
    {$ENDIF}
     {: The current state of this object}
     property    ObjectState: TPerObjectState read GetObjectState write SetObjectState;
